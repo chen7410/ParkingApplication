0b7778f3edbc7f6e5fc2f572cfebec6a1d25d31b
Minqing <chen7410@uw.edu>
2018-08-03 14:47:09 -0700

    f t

---
+++ src/model/Lot.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Lot {
+	private String mLotName;
+	private String mLocation;
+	private int mCapcity;
+	private int mFloors;
+	
+	public Lot(String theLotName, String theLocation, int theCapacity, int theFloors) {
+		this.mLotName = theLocation;
+		this.mLocation = theLocation;
+		this.mCapcity = theCapacity;
+		this.mFloors = theFloors;
+	}
+	
+	public void setLotName(final String theName) {
+		if (theName == null || theName.length() == 0) {
+			throw
+		}
+	}
+	
+	
+}
